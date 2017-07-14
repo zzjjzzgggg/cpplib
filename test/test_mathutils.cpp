767570baaa8a3d401a0f0de2f1c7ddfccd9bf51f
J. Zhao <zzjjzzgggg@gmail.com>
2017-07-14 13:52:05 +0800

    test beta geometric distribution.

--- test/test_mathutils.cpp
+++ test/test_mathutils.cpp
@@ -14,17 +14,16 @@ double pr(int x, double mu, double theta) {
 }
 
 int main(int argc, char *argv[]) {
-    int n = 10;
-    double mu = .2, theta = .01, alpha = mu / theta, beta = (1 - mu) / theta,
-           sum = 0, sum2 = SpecFun::BetaBinomial(0, n, alpha, beta);
+    int n = 15;
+    double mu = .3, theta = .01, alpha = mu / theta, beta = (1 - mu) / theta,
+           sum = SpecFun::BetaBinomial(0, n, alpha, beta);
 
     for (int i = 1; i <= n; i++) {
         double val = SpecFun::BetaGeometric(i, alpha, beta);
         printf("%d: %.6f, %.6f\n", i, val, pr(i, mu, theta));
         sum += val;
-        sum2 += pr(i, mu, theta);
     }
-    printf("\n%.4f, %.4f\n", sum, sum2);
+    printf("\n%.4f\n", sum);
 
     return 0;
 }
