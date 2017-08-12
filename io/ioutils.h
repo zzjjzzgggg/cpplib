20beb1516534d9c3bd4fd358ba3b8b9c77558565
J. Zhao <zzjjzzgggg@gmail.com>
2017-08-12 18:28:15 +0800

    ioutils: allow adding annotation while saving a file

--- io/ioutils.h
+++ io/ioutils.h
@@ -51,8 +51,10 @@ public:
 // vector
 template <typename TVal>
 void saveVec(const std::vector<TVal>& vec, const std::string& filename,
-             const bool echo = true, const std::string& format = "{}\n") {
+             const bool echo = true, const std::string& format = "{}\n",
+             const std::string& anno = "") {
     std::unique_ptr<IOOut> out_ptr = getIOOut(filename);
+    if (!anno.empty()) out_ptr->save(anno);
     for (auto val : vec) out_ptr->save(fmt::format(format, val));
     if (echo) printf("saved to %s\n", filename.c_str());
 }
@@ -75,8 +77,10 @@ std::vector<T> loadVec(const std::string& filename, const int col = 0) {
 template <typename T1, typename T2>
 void savePrVec(const std::vector<std::pair<T1, T2>>& vec,
                const std::string& filename, const bool echo = true,
-               const std::string& format = "{}\t{}\n") {
+               const std::string& format = "{}\t{}\n",
+               const std::string& anno = "") {
     std::unique_ptr<IOOut> out_ptr = getIOOut(filename);
+    if (!anno.empty()) out_ptr->save(anno);
     for (auto& pr : vec)
         out_ptr->save(fmt::format(format, pr.first, pr.second));
     out_ptr->close();
@@ -102,8 +106,10 @@ std::vector<std::pair<T1, T2>> loadPrVec(const std::string& filename,
 template <typename T1, typename T2, typename T3>
 void saveTupleVec(const std::vector<std::tuple<T1, T2, T3>>& vec,
                   const std::string& filename, const bool echo = true,
-                  const std::string& format = "{}\t{}\t{}\n") {
+                  const std::string& format = "{}\t{}\t{}\n",
+                  const std::string& anno = "") {
     std::unique_ptr<IOOut> out_ptr = getIOOut(filename);
+    if (!anno.empty()) out_ptr->save(anno);
     for (auto&& e : vec)
         out_ptr->save(fmt::format(format, std::get<0>(e), std::get<1>(e),
                                   std::get<2>(e)));
@@ -134,8 +140,10 @@ std::vector<std::tuple<T1, T2, T3>> loadTupleVec(const std::string& filename,
 template <typename TKey, typename TVal>
 void saveMap(const std::unordered_map<TKey, TVal>& map,
              const std::string& filename, const bool echo = true,
-             const std::string& format = "{}\t{}\n") {
+             const std::string& format = "{}\t{}\n",
+             const std::string& anno = "") {
     std::unique_ptr<IOOut> out_ptr = getIOOut(filename);
+    if (!anno.empty()) out_ptr->save(anno);
     for (auto& pr : map)
         out_ptr->save(fmt::format(format, pr.first, pr.second));
     if (echo) printf("saved to %s\n", filename.c_str());
@@ -159,8 +167,10 @@ std::unordered_map<TKey, TVal> loadMap(const std::string& filename,
 // set
 template <typename T>
 void saveSet(const std::unordered_set<T>& set, const std::string& filename,
-             const bool echo = true, const std::string& format = "{}\n") {
+             const bool echo = true, const std::string& format = "{}\n",
+             const std::string& anno = "") {
     std::unique_ptr<IOOut> out_ptr = getIOOut(filename);
+    if (!anno.empty()) out_ptr->save(anno);
     for (auto val : set) out_ptr->save(fmt::format(format, val));
     if (echo) printf("saved to %s\n", filename.c_str());
 }
