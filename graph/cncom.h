df18583c35ccd6d2a94aab3882015a71aee2c759
J. Zhao <zzjjzzgggg@gmail.com>
2018-04-01 17:46:05 +0300

    cncom.h: move implementations outside of class

--- graph/cncom.h
+++ graph/cncom.h
@@ -35,120 +35,142 @@ private:
 public:
     std::vector<std::pair<int, int>> nd_cc_vec_;  // node-component pairs
 
-public:
-    SCCVisitor(const Graph& graph) : graph_(graph), rank_(0) {
-        nd_cc_vec_.reserve(graph_.getNodes());
-    }
-
-    void performDFS() {
-        for (auto it = graph_.beginNI(); it != graph_.endNI(); it++) {
-            int v = it->first;
-            if (record_.find(v) == record_.end()) {
-                makeActive(v);
-                while (v != -1) v = explore(v);
-            }
-        }
-        // topology sort
-        std::reverse(nd_cc_vec_.begin(), nd_cc_vec_.end());
-    }
-
+private:
     /**
      * Make vertex v active.
      */
-    void makeActive(const int v, const int parent = -1) {
-        record_[v] = Element(v, ++rank_, graph_[v].getOutDeg());
-        if (parent != -1) record_[v].parent = parent;
-        stack_.push(v);
-    }
+    void makeActive(const int v, const int parent = -1);
 
     /**
      * Explore one step from current vertex v, possible move to another current
      * vertex and calling it v. Return the next vertex to be explored.
      */
-    int explore(const int v) {
-        Element& ve = record_[v];
-        if (ve.edge != ve.deg) {  // v has untagged edge
-            const int u = graph_[v].getOutNbr(ve.edge++);
-            if (record_.find(u) != record_.end()) {  // seen u before
-                int u_rank = record_[u].rank, v_min_rank = record_[ve.min].rank;
-                if (u_rank < v_min_rank) ve.min = u;
-                return v;
-            } else {  // u is presently unseen
-                makeActive(u, v);
-                // u become the current vertex, i.e., the node to be explored
-                return u;
-            }
-        } else {  // all edges from v are tagged, so v matures
-            int u = ve.parent;
-            if (ve.min == v) {  // subtree with root v forms a SCC
-                finish(v);
-            } else {  // the edge from u to v has just matured, make v.min
-                      // visible from u
-                int v_min_rank = record_[ve.min].rank, &u_min = record_[u].min,
-                    u_min_rank = record_[u_min].rank;
-                if (v_min_rank < u_min_rank) u_min = ve.min;
-            }
-            return u;
-        }
-    }
+    int explore(const int v);
 
     /**
      * Remove v and all its successors on the active stack from the tree, and
      * mark them as a SCC.
      */
-    void finish(const int v) {
-        while (true) {
-            int t = stack_.top();
-            stack_.pop();
-            nd_cc_vec_.emplace_back(t, v);
-            record_[t].rank = INT_MAX;
-            record_[t].parent = v;
-            if (t == v) break;
-        }
+    void finish(const int v);
+
+public:
+    SCCVisitor(const Graph& graph) : graph_(graph), rank_(0) {
+        nd_cc_vec_.reserve(graph_.getNodes());
     }
+
+    /**
+     * Perform DFS
+     */
+    void performDFS();
 
     /**
      * Get connected component connections. Keep one representative connection
      * between two SCCs.
      */
-    std::vector<std::pair<int, int>> getCCEdges() {
-        int cur_cc = -1;
-        std::vector<std::pair<int, int>> cc_edge_vec;
-        std::unordered_set<int> discovered_ccs;
-        for (auto& pr : nd_cc_vec_) {
-            int v = pr.first;
-            int cc_from = pr.second;
-            if (cc_from != cur_cc) {
-                cur_cc = cc_from;
-                discovered_ccs.clear();
-            }
-            discovered_ccs.insert(cc_from);
-            for (int d = 0; d < record_[v].deg; d++) {
-                int cc_to = record_[graph_[v].getOutNbr(d)].parent;
-                if (discovered_ccs.find(cc_to) == discovered_ccs.end()) {
-                    discovered_ccs.insert(cc_to);
-                    cc_edge_vec.emplace_back(cc_from, cc_to);
-                }
-            }
-        }
-        return cc_edge_vec;
-    }
+    std::vector<std::pair<int, int>> getCCEdges() const;
 
     /**
      * Return topologically sorted CCs
      */
-    std::vector<int> getCCSorted() {
-        std::vector<int> vec;
-        int cc = -1;
-        for (auto& pr: nd_cc_vec_) {
-            if (pr.second != cc) {
-                cc = pr.second;
-                vec.push_back(cc);
+    std::vector<int> getCCSorted() const;
+};
+
+template <class Graph>
+void SCCVisitor<Graph>::makeActive(const int v, const int parent) {
+    record_[v] = Element(v, ++rank_, graph_[v].getOutDeg());
+    if (parent != -1) record_[v].parent = parent;
+    stack_.push(v);
+}
+
+template <class Graph>
+int SCCVisitor<Graph>::explore(const int v) {
+    Element& ve = record_[v];
+    if (ve.edge != ve.deg) {  // v has untagged edge
+        const int u = graph_[v].getOutNbr(ve.edge++);
+        if (record_.find(u) != record_.end()) {  // seen u before
+            int u_rank = record_[u].rank, v_min_rank = record_[ve.min].rank;
+            if (u_rank < v_min_rank) ve.min = u;
+            return v;
+        } else {  // u is presently unseen
+            makeActive(u, v);
+            // u become the current vertex, i.e., the node to be explored
+            return u;
+        }
+    } else {  // all edges from v are tagged, so v matures
+        int u = ve.parent;
+        if (ve.min == v) {  // subtree with root v forms a SCC
+            finish(v);
+        } else {  // the edge from u to v has just matured, make v.min
+                  // visible from u
+            int v_min_rank = record_[ve.min].rank, &u_min = record_[u].min,
+                u_min_rank = record_[u_min].rank;
+            if (v_min_rank < u_min_rank) u_min = ve.min;
+        }
+        return u;
+    }
+}
+
+template <class Graph>
+void SCCVisitor<Graph>::finish(const int v) {
+    while (true) {
+        int t = stack_.top();
+        stack_.pop();
+        nd_cc_vec_.emplace_back(t, v);
+        record_[t].rank = INT_MAX;
+        record_[t].parent = v;
+        if (t == v) break;
+    }
+}
+
+template <class Graph>
+void SCCVisitor<Graph>::performDFS() {
+    for (auto it = graph_.beginNI(); it != graph_.endNI(); it++) {
+        int v = it->first;
+        if (record_.find(v) == record_.end()) {
+            makeActive(v);
+            while (v != -1) v = explore(v);
+        }
+    }
+    // topology sort
+    std::reverse(nd_cc_vec_.begin(), nd_cc_vec_.end());
+}
+
+template <class Graph>
+std::vector<std::pair<int, int>> SCCVisitor<Graph>::getCCEdges() const {
+    int cur_cc = -1;
+    std::vector<std::pair<int, int>> cc_edge_vec;
+    std::unordered_set<int> discovered_ccs;
+    for (auto& pr : nd_cc_vec_) {
+        int v = pr.first;
+        int cc_from = pr.second;
+        if (cc_from != cur_cc) {
+            cur_cc = cc_from;
+            discovered_ccs.clear();
+        }
+        discovered_ccs.insert(cc_from);
+        for (int d = 0; d < record_.at(v).deg; d++) {
+            int cc_to = record_.at(graph_[v].getOutNbr(d)).parent;
+            if (discovered_ccs.find(cc_to) == discovered_ccs.end()) {
+                discovered_ccs.insert(cc_to);
+                cc_edge_vec.emplace_back(cc_from, cc_to);
             }
         }
-        return vec;
     }
-};
+    return cc_edge_vec;
+}
+
+template <class Graph>
+std::vector<int> SCCVisitor<Graph>::getCCSorted() const {
+    std::vector<int> vec;
+    int cc = -1;
+    for (auto& pr : nd_cc_vec_) {
+        if (pr.second != cc) {
+            cc = pr.second;
+            vec.push_back(cc);
+        }
+    }
+    return vec;
+}
 
 } /* namespace graph */
 #endif /* __CNCOM_H__ */
