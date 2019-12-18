61038d61c7a61bbd650e8d0193cb7504529571ae
JZ <zzjjzzgggg@gmail.com>
2019-12-18 21:10:22 +0800

    triad: remove useless header; use prefix

--- graph/triad.h
+++ graph/triad.h
@@ -8,7 +8,6 @@
 
 #include <vector>
 #include <unordered_map>
-#include <algorithm>
 
 namespace graph {
 /**
@@ -18,25 +17,28 @@ template <class Graph>
 int countNodeDirTriads(const int& node, const Graph& G) {
     if (G[node].getDeg() < 2) return 0;
     int triads = 0;
+
     // get unique neighbors
     std::unordered_map<int, int> nbr_cnt;
-    const auto& nd_obj = G[node];
+    auto&& nd_obj = G[node];
     for (auto ni = nd_obj.beginNbr(); ni != nd_obj.endNbr();
          nd_obj.nextNbr(ni)) {
         int nbr = nd_obj.getNbrID(ni);
         if (nbr != node) nbr_cnt[nbr]++;
     }
+
     std::vector<int> nbr_vec;
     nbr_vec.reserve(nbr_cnt.size());
     for (auto& it : nbr_cnt) nbr_vec.push_back(it.first);
+
     // count connected neighbors
-    for (auto src_it = nbr_vec.begin(); src_it != nbr_vec.end(); src_it++) {
+    for (auto src_it = nbr_vec.begin(); src_it != nbr_vec.end(); ++src_it) {
         auto& src_obj = G[*src_it];
         std::unordered_map<int, int> src_nbr_cnt;
         for (auto ni = src_obj.beginNbr(); ni != src_obj.endNbr();
              src_obj.nextNbr(ni))
             src_nbr_cnt[src_obj.getNbrID(ni)]++;
-        for (auto dst_it = src_it + 1; dst_it != nbr_vec.end(); dst_it++) {
+        for (auto dst_it = src_it + 1; dst_it != nbr_vec.end(); ++dst_it) {
             if (src_nbr_cnt.find(*dst_it) != src_nbr_cnt.end())
                 triads +=
                     src_nbr_cnt[*dst_it] * nbr_cnt[*src_it] * nbr_cnt[*dst_it];
